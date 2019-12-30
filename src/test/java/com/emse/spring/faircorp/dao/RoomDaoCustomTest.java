17256044fc0436c79699e95db1c978439cf6e5da
mohamediderrissi <mohamediderrissi@gmail.com>
2019-12-30 22:38:25 +0100

    commit__

--- src/test/java/com/emse/spring/faircorp/dao/RoomDaoCustomTest.java
+++ src/test/java/com/emse/spring/faircorp/dao/RoomDaoCustomTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
